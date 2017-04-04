5dbcbca86050b014fcf82dddd09d4f3ebff5c2d7
Jesse Prescott <jesseprescott@yahoo.com>
2017-04-04 16:04:35 -0400

    Added routes

---
+++ app/Http/Controllers/PasswordController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PasswordController extends Controller
+{
+  /**
+   * GET
+   */
+    public function index(){
+      return 'Hello';
+    }
+}
